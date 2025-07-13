f16ee66d67c7370c5c84eea52457ba09e296feaa
TruongHai <zapter0000@gmail.com>
2025-07-13 14:45:39 +0700

    Fix lỗi MainController

--- src/java/controller/MainController.java
+++ src/java/controller/MainController.java
@@ -70,7 +70,7 @@ public class MainController extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        PrintWriter out = response.getWriter();
+        processRequest(request, response);
     }
 
     @Override
